723101021103d20376a4be5c90484cd3854ec022
Joelute <Josephho678@gmail.com>
2022-01-13 21:30:54 +0000

    Intergrate Discord O-Auth

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,8 @@
 import type { Handle } from "@sveltejs/kit";
+import type { GetSession } from "@sveltejs/kit";
+import cookie from 'cookie';
+const DISCORD_API = import.meta.env.VITE_DISCORD_API;
+const HOST = import.meta.env.VITE_HOST
 
 export const handle: Handle = async ({request, resolve}) => {
     if (request.query.has("_method")) {
@@ -9,3 +13,49 @@ export const handle: Handle = async ({request, resolve}) => {
     return response;
     
 };
+
+export const getSession: GetSession = async (request) => {
+    const cookies = cookie.parse(request.headers.cookie || "");
+
+    if (cookies.disco_refresh_token && !cookies.disco_access_token) {
+        const discord_request = await fetch(`${HOST}/api/refresh?code=${cookies.disco_refresh_token}`);
+
+        const discord_response = await discord_request.json();
+        
+        if (discord_response.disco_access_token) {
+            console.log('setting discord user via refresh token..')
+            const request = await fetch(`${DISCORD_API}/users/@me`, {
+                headers: { 'Authorization': `Bearer ${discord_response.disco_access_token}` }
+            });
+            const response = await request.json();
+
+            if (response.id){
+                return{
+                    user: {
+                        ...response
+                    }
+                }
+            }
+        }
+    }
+/*
+    if (cookies.disco_access_token) {
+        console.log('setting discord user via access token...')
+        const request = await fetch(`${DISCORD_API}/users/@me`, {
+            headers: { 'Authorization': `Bearer ${cookies.disco_access_token}` }
+        });
+        const response = await request.json();
+
+        if (response.id){
+            return{
+                user: {
+                    ...response
+                }
+            }
+        }
+    }
+*/
+    return {
+        user: false
+    }
+}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,32 @@
+<script context="module" lang="ts">
+    import type { Load } from "@sveltejs/kit";
+    /*
+    export const load: Load = async ({ session }) => {
+        return {
+            props: { user: session.user || false } 
+        }
+    }
+    */
+</script>
+
 <script>
     import "../app.css";
 </script>
 
+
+
 <nav class = "nav-bar">
-    <div class = "nav-bar-list">
-        <a href= "./" class="nav-bar-list-home">Home</a>
-        <a href= "./about-us" class = "nav-bar-list-about">About Us</a>
-        <a href= "./" class = "nav-bar-list-todo">To Do</a>
+    <ul class = "nav-links">
+        <li>
+            <a href= "./">Home</a>
+        </li>
+        <li>
+            <a href= "./about-us">About</a>
+        </li>
+    </ul>
+    <div class = "login-button"> 
+        <a href="https://discord.com/api/oauth2/authorize?client_id=930626088135196742&redirect_uri=https%3A%2F%2F3000-peach-coyote-w3aqp3gu.ws-us27.gitpod.io%2Fapi%2Fcallback&response_type=code&scope=identify">Login</a>
     </div>
+
 </nav>
 <slot />
